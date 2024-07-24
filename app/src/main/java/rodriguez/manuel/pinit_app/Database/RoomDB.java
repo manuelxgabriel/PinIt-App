23a163323b4bda8b46ea49803a07fb73f6a27b7d
Manuel <manuelxgabriel@gmail.com>
2024-07-23 18:15:03 -0700

    Add all of the layout for the notes

---
+++ app/src/main/java/rodriguez/manuel/pinit_app/Database/RoomDB.java
@@ -0,0 +1,48 @@
+package rodriguez.manuel.pinit_app.Database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import rodriguez.manuel.pinit_app.Models.Notes;
+
+/**
+ * Represent the Room database for the PINITAPP application.
+ * This is a singleton class that provides access to the database
+ */
+@Database(entities = Notes.class, version = 1, exportSchema = false)
+public abstract class RoomDB extends RoomDatabase {
+    private static RoomDB database;
+    private static String DATABASE_NAME = "PINITAPP";
+
+    /**
+     * Provides a singleton instance of the RoomDB database.
+     * Ensures that only one instance of the database is created
+     * throughout the application.
+     *
+     * @param context The context of the application.
+     * @return the singleton instance of the RoomDB database
+     */
+    public synchronized static RoomDB getInstance(Context context){
+        if(database == null){
+            database = Room.databaseBuilder(context.getApplicationContext(),
+            RoomDB.class, DATABASE_NAME)
+                    .allowMainThreadQueries() // Allow queries on the main thread
+                    .fallbackToDestructiveMigration() // Handle schema migration
+                                                    // by destroying and recreating the database
+                    .build();
+        }
+        return database;
+    }
+
+
+    /**
+     * Provides access to the MainDAO data access object.
+     * This method allows interaction with the Notes data operations.
+     * @return the MainDAO instance for database operations.
+     */
+    public abstract MainDAO mainDAO();
+
+}
