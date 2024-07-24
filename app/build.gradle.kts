993246a69b5529e9cdd50e453367a3dbe324e3d3
Manuel <manuelxgabriel@gmail.com>
2024-07-23 22:17:54 -0700

    Added the RoomDB Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Room Database
+    implementation(libs.room.runtime)
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
 }
